f7c4e86156d6f8775e41588a5a7091affccc306e
Roberto0611 <129466817+Roberto0611@users.noreply.github.com>
2023-12-08 23:30:15 +0000

    Finish class return (ticket)

--- budget.py
+++ budget.py
@@ -4,11 +4,11 @@
 class Category:
   name = "Default"
   total = 0
-  ledger = []
-  
+
   #Init
   def __init__(self, name):
     self.name = name
+    self.ledger = []
 
   #Method deposit
   def deposit(self, amount, description=""):
@@ -16,8 +16,8 @@ class Category:
     self.ledger.append({"amount": amount, "description": description})
 
   #Method withdraw
-  def withdraw(self, amount , description=""):
-    if self.check_funds(amount) is False :
+  def withdraw(self, amount, description=""):
+    if self.check_funds(amount) is False:
       return False
     else:
       self.total = self.total - amount
@@ -30,13 +30,13 @@ class Category:
 
   #Method tranfer
   def transfer(self, amount, destination):
-    description = 'Transfer to' + destination.name
+    description = 'Transfer to ' + destination.name
     if self.withdraw(amount, description) is True:
-      description2 = "Transfer from" + destination.name
+      description2 = "Transfer from " + self.name
       destination.deposit(amount, description2)
     else:
       return False
-  
+
   #Method check_funds
   def check_funds(self, amount):
     if amount > self.total:
@@ -44,7 +44,25 @@ class Category:
     elif amount < self.total:
       return True
 
-  
+  # Method to format ledger entries
+  def format_ledger_entry(self, entry):
+    return f"{entry['description'][:23]:<23} {entry['amount']:.2f}"
 
+  # Method __str__
+  def __str__(self):
+    # Create the title line
+    title_line = f"{'*' * ((30 - len(self.name)) // 2)}{self.name}{'*' * ((30 - len(self.name)) // 2)}"
+
+    # Create the ledger entries
+    ledger_entries = "\n".join(
+        self.format_ledger_entry(entry) for entry in self.ledger)
+
+    # Create the total line
+    total_line = f"\nTotal: {self.total:.2f}"
+
+    # Combine all parts into the final string
+    result = f"{title_line}\n{ledger_entries}{total_line}"
+
+    return result
 
 #def create_spend_chart(categories):
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 # This entrypoint file to be used in development. Start by reading README.md
 import budget
-#from budget import create_spend_chart
+from budget import create_spend_chart
 #from unittest import main
 
 food = budget.Category('food')
@@ -10,20 +10,18 @@ food.withdraw(10.15, "groceries")
 food.withdraw(15.89, "restaurant and more food for dessert")
 print("food:" , food.get_balance())
 clothing = budget.Category("Clothing")
-print("clothing:" , clothing.get_balance())
 food.transfer(50, clothing)
-print("new Food:", food.get_balance())
-print("new clothing:" , clothing.get_balance())
 clothing.withdraw(25.55)
 clothing.withdraw(100)
 auto = budget.Category("Auto")
 auto.deposit(1000, "initial deposit")
 auto.withdraw(15)
 
+
 print(food)
 print(clothing)
 
-#print(create_spend_chart([food, clothing, auto]))
+print(create_spend_chart([food, clothing, auto]))
 
 # Run unit tests automatically
 #main(module='test_module', exit=False)
