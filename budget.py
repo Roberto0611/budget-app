31424c143ac0552c72e9f3e6efe61b104a9a6173
Roberto0611 <129466817+Roberto0611@users.noreply.github.com>
2023-12-08 23:15:44 +0000

    Finish Main logic

--- budget.py
+++ budget.py
@@ -1,7 +1,50 @@
+#Script by: Roberto Ochoa Cuevas
+
+#category budget
 class Category:
+  name = "Default"
+  total = 0
+  ledger = []
+  
+  #Init
+  def __init__(self, name):
+    self.name = name
+
+  #Method deposit
+  def deposit(self, amount, description=""):
+    self.total = amount + self.total
+    self.ledger.append({"amount": amount, "description": description})
+
+  #Method withdraw
+  def withdraw(self, amount , description=""):
+    if self.check_funds(amount) is False :
+      return False
+    else:
+      self.total = self.total - amount
+      self.ledger.append({"amount": -amount, "description": description})
+      return True
+
+  #Method get_balance
+  def get_balance(self):
+    return self.total
+
+  #Method tranfer
+  def transfer(self, amount, destination):
+    description = 'Transfer to' + destination.name
+    if self.withdraw(amount, description) is True:
+      description2 = "Transfer from" + destination.name
+      destination.deposit(amount, description2)
+    else:
+      return False
+  
+  #Method check_funds
+  def check_funds(self, amount):
+    if amount > self.total:
+      return False
+    elif amount < self.total:
+      return True
+
+  
 
 
-
-
-
-def create_spend_chart(categories):
+#def create_spend_chart(categories):
--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 # This entrypoint file to be used in development. Start by reading README.md
 import budget
-from budget import create_spend_chart
-from unittest import main
+#from budget import create_spend_chart
+#from unittest import main
 
-food = budget.Category("Food")
+food = budget.Category('food')
 food.deposit(1000, "initial deposit")
+
 food.withdraw(10.15, "groceries")
 food.withdraw(15.89, "restaurant and more food for dessert")
-print(food.get_balance())
+print("food:" , food.get_balance())
 clothing = budget.Category("Clothing")
+print("clothing:" , clothing.get_balance())
 food.transfer(50, clothing)
+print("new Food:", food.get_balance())
+print("new clothing:" , clothing.get_balance())
 clothing.withdraw(25.55)
 clothing.withdraw(100)
 auto = budget.Category("Auto")
@@ -19,7 +23,7 @@ auto.withdraw(15)
 print(food)
 print(clothing)
 
-print(create_spend_chart([food, clothing, auto]))
+#print(create_spend_chart([food, clothing, auto]))
 
 # Run unit tests automatically
-main(module='test_module', exit=False)
+#main(module='test_module', exit=False)
